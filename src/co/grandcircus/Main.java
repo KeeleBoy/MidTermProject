709288d658eb042ea3363d81be1568634ccb61f4
kyle <kwarchuck@gmail.com>
2020-02-11 14:00:40 -0500

    boos stuff added

--- src/co/grandcircus/Main.java
+++ src/co/grandcircus/Main.java
@@ -2,15 +2,14 @@ package co.grandcircus;
 
 public class Main {
 
+	static FileHelper<Book> fileHelper = new FileHelper<>("Books.txt", (new BookLineConverter()));
+
 	/*
-	 * @Authors Sam Keim, James McDowell and Kyle Warchuck
-	 *  test
+	 * @Authors Sam Keim, James McDowell and Kyle Warchuck test
 	 * 
 	 */
 
 	public static void main(String[] args) {
-		
-		
 
 	}
 
