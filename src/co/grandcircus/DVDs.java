6336815f51fedd64d96c3c91ea37f60c08fe4aaf
James <jamesmcdowell1897@gmail.com>
2020-02-11 14:00:21 -0500

    created DVD.txt

--- src/co/grandcircus/DVDs.java
+++ src/co/grandcircus/DVDs.java
@@ -1,7 +1,24 @@
 package co.grandcircus;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class DVDs {
 	
 	static FileHelper<DVD> fileHelper = new FileHelper<>("DVDs.txt", (new DVDLineConverter()));
-
+	
+	
+	
+	
+	public static void DVDAdd() {
+		
+		 DateTimeFormatter format = DateTimeFormatter.ofPattern("M/d/yy");
+		 String dueDate = "3/1/20";
+		 LocalDate setDueDate = LocalDate.parse(dueDate, format);
+		
+		 DVD dvd1 = new DVD("The Godfather", false, setDueDate , 178, "Francis Ford Coppola");
+		
+		Main.fileHelper.append(dvd1);
+	}
+	
 }
--- src/co/grandcircus/Main.java
+++ src/co/grandcircus/Main.java
@@ -1,17 +1,13 @@
 package co.grandcircus;
 
 public class Main {
-
+	static FileHelper<DVD> fileHelper = new FileHelper<>("DVDs.txt", (new DVDLineConverter()));
 	/*
 	 * @Authors Sam Keim, James McDowell and Kyle Warchuck
 	 *  test
 	 * 
 	 */
 
-	public static void main(String[] args) {
-		
-		
-
-	}
+	
 
 }
