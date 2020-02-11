c3051ed8308caa31dc7849162dc73e5b2b2b2f99
James <jamesmcdowell1897@gmail.com>
2020-02-11 18:30:02 -0500

    updated with comments

--- src/co/grandcircus/DVDLineConverter.java
+++ src/co/grandcircus/DVDLineConverter.java
@@ -14,28 +14,28 @@ public class DVDLineConverter implements LineConverter<DVD> {
 	@Override
 	public DVD fromLine(String line) {
 
-		line.split("\t");
+		line.split("\t"); // creates a means of splitting lines at tab at tab
 		
 		boolean status;
 		String dueDate = "";
 
-		String[] lines = line.split("\t");
-		String title = lines[0];
-		if (lines[1].equals("true")) {
-			status = true;
+		String[] lines = line.split("\t"); //actually splits the lines into an array
+		String title = lines[0];// first segment becomes the title
+		if (lines[1].equals("true")) {//if then statement for the status
+			status = true; // if line reads true, then the status becomes true
 		} else {
-			status = false;
+			status = false; // otherwise status becomes false
 		}
 		
-		DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-dd-MM");
+		DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-dd-MM"); // pulls the 3rd segment and creates a Date variable
 		dueDate = lines[2];
 		LocalDate setDueDate = LocalDate.parse(dueDate, format);
 		
 		
-		int runtime = Integer.parseInt(lines[3]);
-		String director = lines[4];
+		int runtime = Integer.parseInt(lines[3]); // pulls the 4th segment for the runtime portion of the DVD
+		String director = lines[4]; // pulls the 5th segment for the director
 
-		return new DVD(title, status, setDueDate, runtime, director);
+		return new DVD(title, status, setDueDate, runtime, director); // inputs all new variables into a new DVD
 	}
 
 }
