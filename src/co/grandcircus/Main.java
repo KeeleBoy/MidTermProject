243682996cc3014eda1828f06048e40b04a975d5
Sam Keim <skeim235@gmail.com>
2020-02-11 14:14:00 -0500

    Midstep

--- src/co/grandcircus/Main.java
+++ src/co/grandcircus/Main.java
@@ -18,8 +18,9 @@ public class Main {
 		do{
 			mm.printMenu();
 			System.out.println("\nEnter a selection:");
-			// FIXME get Library array
-			userChoice = Validator.getInt(scnr, 1, 3);
+			// FIXME James get DVD array
+			// FIXME Kyle get book array
+			userChoice = Validator.getInt(scnr, 1, 4);
 			switch(userChoice) {
 			case 1:
 				// Display
@@ -32,6 +33,7 @@ public class Main {
 			break;
 			default:
 				userContinue = false;
+				System.out.println("Thank you for visiting. Goodybe.");
 			}
 		} while (userContinue);
 	}
