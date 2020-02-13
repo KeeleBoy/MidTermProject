66eaa9a24e731b03f569026fa5b9c2ae0aa80f49
kyle <kwarchuck@gmail.com>
2020-02-13 14:08:58 -0500

    pulling

--- src/co/grandcircus/AudioBookLineConverter.java
+++ src/co/grandcircus/AudioBookLineConverter.java
@@ -4,8 +4,8 @@ public class AudioBookLineConverter implements LineConverter<AudioBook> {
 
 	@Override
 	public String toLine(AudioBook object) {
-		return String.format("%s\t%s\t%s\t%s\t%s", object.getTitle(), object.isCheckedOut(),
-				object.getDueDate(), object.getRuntime(), object.getAuthor());
+		return String.format("%s\t%s\t%s\t%s\t%s", object.getTitle(), object.isCheckedOut(), object.getDueDate(),
+				object.getRuntime(), object.getAuthor());
 	}
 
 	@Override
@@ -16,7 +16,7 @@ public class AudioBookLineConverter implements LineConverter<AudioBook> {
 
 		String[] lines = line.split("\t"); // actually splits the lines into an array
 		String title = lines[0];// first segment becomes the title
-		String author = lines[4];
+		String author = lines[1];
 		if (lines[1].equals("true")) {// if then statement for the status
 			status = true; // if line reads true, then the status becomes true
 		} else {
@@ -25,7 +25,7 @@ public class AudioBookLineConverter implements LineConverter<AudioBook> {
 
 		int runtime = Integer.parseInt(lines[3]); // pulls the 4th segment for the runtime portion of the DVD
 
-		return new AudioBook(title, status, runtime, author); // inputs all new variables into a new DVD
+		return new AudioBook(author, status, runtime, title); // inputs all new variables into a new DVD
 	}
 
 }
